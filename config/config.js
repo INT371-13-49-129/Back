8557f25ea056ba926b8ffea709c24038c686c68a
Patiphan <patiphan.new@mail.kmutt.ac.th>
2022-05-26 22:15:56 +0700

    [FIX] compose config.js

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 
 config = {
   port: process.env.PORT || 3000,
-  host: process.env.HOST || "yenjai.3utilities.com",
+  host: process.env.HOST || "jaidee.3utilities.com",
   user: process.env.USER_DB || "root",
   port_db: process.env.PORT_DB || "3306",
   password: process.env.PASS_DB || "kamin124712",
   database: process.env.NAME_DB || "jaid_db",
-  front_url: process.env.FRONT_URL || "yenjai.3utilities.com",
+  front_url: process.env.FRONT_URL || "jaidee.3utilities.com",
   mail_pass:  process.env.MAIL_PASS || "rotmpdzovxpdwiom",
 };
 module.exports = config;
