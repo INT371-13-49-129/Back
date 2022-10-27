301d71783f3778a25ab5cc69c7e1b45908c81862
Patiphan Thiansuwan <50895555+Patiphannew@users.noreply.github.com>
2022-10-27 17:49:36 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ require("dotenv").config();
 
 config = {
   port: process.env.PORT || 3000,
-  host: process.env.HOST || "52.141.58.2",
+  host: process.env.HOST || "20.2.208.26",
   user: process.env.USER_DB || "root",
   port_db: process.env.PORT_DB || "3306",
   password: process.env.PASS_DB || "kamin124712",
   database: process.env.NAME_DB || "jaid_db",
-  front_url: process.env.FRONT_URL || "https://jaid.onthewifi.com",
+  front_url: process.env.FRONT_URL || "https://jaid.serveblog.net",
   mail_pass:  process.env.MAIL_PASS || "rotmpdzovxpdwiom",
 };
 module.exports = config;
